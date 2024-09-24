222591380a8b357a90204ec0acf7bb16e1724234
Prabhjot Singh <prabhjotsingh1032003@gmail.com>
2024-09-24 15:43:07 +0530

    some changes in company routes and testing api registercompany,get and update comapnies

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,8 +1,20 @@
 import { Company } from "../models/company.model.js";
-
+import { User } from "../models/user.Model.js";
 export const registerCompany = async(req,res)=>{
    try {
      const {name} = req.body;
+   //   checking user is recuriter or student 
+   //   const userId = req._id;
+  
+   //    const checkRecuriter = await User.findOne({userId})
+   //   console.log(checkRecuriter)
+   //   if(checkRecuriter.role === "student")
+   //   {
+   //    return res.status(400).json({
+   //       message:"Student cannont create company",
+   //       success:false
+   //    })
+   //   }
      if(!name)
      { 
           return res.status(400).json({
@@ -39,12 +51,64 @@ export const getCompany = async(req,res)=>{
         const companies =  await Company.find({userId});
         if(!companies)
         {
-             return res.status(404).json{
-                message:""
-             }
+             return res.status(404).json({
+                message:"Companies",
+                success:false })
         }
+        return res.status(200).json({
+         companies,
+         success:true
+        })
     } catch (error) {
-        
+     console.log(error)    
     }
 }
 
+// getCompanybyId
+export const getCompanybyId = async(req,res) =>{
+   try {
+      const companyId = req.params.id;
+      const company = await Company.findById(companyId);
+      // console.log(company)
+      if(!company)
+      {
+          return res.status(404).json({
+            messasge:"Company not found",
+            success:false
+          })
+      }
+      return res.status(200).json({
+         company,
+         success:true
+      })
+   } catch (error) {
+      return res.status(404).json({
+         messasge:"Id invalid ",
+         success:false
+       })
+   }
+}
+
+export const updateCompany = async(req,res) =>{
+   try {
+      const {name,description,website,location} = req.body;
+      const file = req.file;
+      // cloudinary
+      const updateData = {name,description,website,location};
+      const company = await Company.findByIdAndUpdate(req.params.id,updateData,{name:true})
+   
+      if(!company)
+      {
+          return res.status(404).json({
+            message:"Company not found",
+            success:false
+          })
+      }
+      return res.status(200).json({
+         message:"Company updated successfully",
+         success:true
+      })
+   } catch (error) {
+     console.log(error) 
+   } 
+}
--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import dotenv from "dotenv"
 import connectDB from "./utils/db.js"; 
 import userRoute from "./routes/user.route.js";
+import companyRoute from './routes/company.routes.js'
 dotenv.config({})
 const app = express()
 
@@ -21,6 +22,8 @@ const PORT = process.env.PORT||3000;
 
 app.use("/api/v1/user",userRoute)
 // http://localhost:3000/api/v1/user/register
+app.use("/api/v1/company",companyRoute)
+
 app.listen(PORT,()=>{
      console.log(`Server is running on port ${PORT}`)
      connectDB()
