2e219b24f904d4c60c10718f596e17de39d8f2fe
Prabhjot Singh <prabhjotsingh1032003@gmail.com>
2024-10-05 18:09:00 +0530

    set up routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import Layout from "./Components/Layout/Layout.jsx";
+import Login from "./Components/auth/Login.jsx";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" />
+      <Route path="login" element={<Login />} />
+    </Route>
+  )
+);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
