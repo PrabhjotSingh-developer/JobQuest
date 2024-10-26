6f89a6afec642f4f98101ff7e7988b2ca9c47549
Prabhjot Singh <prabhjotsingh1032003@gmail.com>
2024-10-26 21:19:09 +0530

    rebuilding slider

--- frontend/src/Components/CategoryCarousel/CategoryCarousel.jsx
+++ frontend/src/Components/CategoryCarousel/CategoryCarousel.jsx
@@ -12,18 +12,14 @@ const CategoryCarousel = () => {
     "Ui/Ux designer",
   ];
   const [slide,setSlide] = useState(0)
-  function clickhandler (e){
-
-    e.preventDefault()
+  function nextClick(){
+    const item = document.querySelector(".maindiv");
    
-     const item = document.querySelector(".maindiv");
-    const btn = e.target.value;
-    console.log(btn)
+   
     const size = window.screen.width;
   
     
-    if(btn === "Right" && size > 540 ) 
-    {
+  
       if(slide<2)
         {
         setSlide(slide+1);
@@ -38,9 +34,15 @@ const CategoryCarousel = () => {
           item.style.transform = `translateX(0px)`
           
       }
-    }
-    else 
-    {
+    
+  }
+  function prevClick (){
+
+    
+    const item = document.querySelector(".maindiv");
+ 
+
+
       if(slide>0 )
         {
           item.style.transition = ".5s linear"
@@ -55,29 +57,13 @@ const CategoryCarousel = () => {
         setSlide(2);
           
       }
-    }
+    
   
        
   }
   return (
     <div className="w-[100%] mt-4 overflow-hidden" >
-       <h1 className="text-2xl font-bold text-center my-4">Job Categories</h1>
-      <div className="w-[800px] overflow-x-hidden flex justify-evenly  mx-auto md:h-[100px] items-center">
-        <span className="md:absolute left-2  cursor-pointer hidden md:flex" onClick={clickhandler} name={"Left"} ><FaArrowLeft /></span>
-        <div className="w-[100%]  px-[20px] overflow-scroll h-[100%] flex items-center " style={{scrollbarWidth:"none"}}>
-
-          <div className="maindiv   md:relative flex md:flex-row flex-col gap-5 md:gap-10 items-center justify-center" >
-            {catCar.map((item, index) => (
-
-              <div className={`sliderItems md:absolute static flex  w-[100%]  md:w-[300px]  `}   style={{ left: `${index * 400}px` }}>
-                
-               <span><FaStar/></span> <NavLink className={"font-semibold"}>{item}</NavLink>
-              </div>
-            ))}
-          </div>
-        </div>
-        <span className="md:absolute right-2  cursor-pointer hidden md:flex" onClick={clickhandler} name={"Right"}><FaArrowRight /></span>
-      </div>
+  
     </div>
   );
 };
