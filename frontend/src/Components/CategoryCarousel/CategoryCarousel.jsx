2e1b2adb93c82e3004e95f1de40a5ff130feb6dc
Prabhjot Singh <prabhjotsingh1032003@gmail.com>
2024-11-07 21:02:52 +0530

    setting up the slider

--- frontend/src/Components/CategoryCarousel/CategoryCarousel.jsx
+++ frontend/src/Components/CategoryCarousel/CategoryCarousel.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
-import { FaStar ,FaArrowRight,FaArrowLeft} from "react-icons/fa";
+import { FaStar, FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const CategoryCarousel = () => {
   const catCar = [
     "Frontend Devloper",
@@ -11,59 +12,50 @@ const CategoryCarousel = () => {
     "Data Science",
     "Ui/Ux designer",
   ];
-  const [slide,setSlide] = useState(0)
-  function nextClick(){
+  const [slide, setSlide] = useState(0);
+  function nextClick() {
     const item = document.querySelector(".maindiv");
-   
-   
+
     const size = window.screen.width;
-  
-    
-  
-      if(slide<2)
-        {
-        setSlide(slide+1);
-        item.style.transition = ".5s linear"
-        console.log(slide)
-        item.style.transform = `translateX(${slide==0?"-800px":-((slide+1)*800)+"px"})`
-      }
-      else
-      {
-          setSlide(0);
-          item.style.transition = "none"
-          item.style.transform = `translateX(0px)`
-          
-      }
-    
+
+    if (slide < 2) {
+      setSlide(slide + 1);
+      item.style.transition = ".5s linear";
+      console.log(slide);
+      item.style.transform = `translateX(${
+        slide == 0 ? "-800px" : -((slide + 1) * 800) + "px"
+      })`;
+    } else {
+      setSlide(0);
+      item.style.transition = "none";
+      item.style.transform = `translateX(0px)`;
+    }
   }
-  function prevClick (){
-
-    
+  function prevClick() {
     const item = document.querySelector(".maindiv");
- 
 
+    if (slide > 0) {
+      item.style.transition = ".5s linear";
 
-      if(slide>0 )
-        {
-          item.style.transition = ".5s linear"
-         
-          setSlide(slide-1);
-          item.style.transform = `translateX(${slide === 1? "0px":(slide-1)*-800+"px"})`
-      }
-      else
-      {
-        item.style.transition = "none"
-        item.style.transform = `translateX(${-1600}px)`
-        setSlide(2);
-          
-      }
-    
-  
-       
+      setSlide(slide - 1);
+      item.style.transform = `translateX(${
+        slide === 1 ? "0px" : (slide - 1) * -800 + "px"
+      })`;
+    } else {
+      item.style.transition = "none";
+      item.style.transform = `translateX(${-1600}px)`;
+      setSlide(2);
+    }
   }
   return (
-    <div className="w-[100%] mt-4 overflow-hidden" >
-  
+    <div className="w-[100%] mt-4 flex items-center justify-center ">
+      <div className="grid grid-cols-6 gap-10 overflow-scroll w-[80%] mx-auto absolute bottom-0">
+        {catCar.map((item, index) => (
+          <div className={`grid-items bg-red-400 w-[300px] top-0  absolute`} style={{left:`${index*300}px`}}>
+            <Link>{item}</Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
